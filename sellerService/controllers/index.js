c9a724d1f4b9117596f963f9957757182bfb458a
petruceles <petrusprihantoro@gmail.com>
2022-05-02 21:30:25 +0700

    start controller development

---
+++ sellerService/controllers/index.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+
+const {
+    DB_USERNAME,
+    DB_PASSWORD,
+    DB_DATABASE,
+    DB_HOST,
+    DB_DIALECT
+    } = process.env
+
+const {Seller} = require('../models');
+
+module.exports = {
+    
+}
