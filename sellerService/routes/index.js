9181af3e41f3be63aec085f77dbbc130bb2f3422
petruceles <petrusprihantoro@gmail.com>
2022-05-02 22:40:35 +0700

    completed seller service

--- sellerService/routes/index.js
+++ sellerService/routes/index.js
@@ -1,9 +1,7 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const seller = require('./sellers');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.use('/sellers', seller);
 
 module.exports = router;
--- sellerService/routes/sellers.js
+++ sellerService/routes/sellers.js
@@ -1,9 +1,12 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+const seller = require('../controllers');
+
+router.get('/', seller.index);
+router.get('/:id', seller.detail);
+router.post('/', seller.register);
+router.put('/:id', seller.update);
+router.delete('/:id', seller.delete);
 
 module.exports = router;
